714dfc4b11b58f340802cc0c2299a8b5518fb0d6
SilverParsnip96 <39203604@uaz.edu.mx>
2022-10-23 16:57:34 -0500

    implementación de calendario

---
+++ Codigo/app/src/main/java/com/softcare/pockettrainer/CalendarAdapter.java
@@ -0,0 +1,2 @@
+package com.softcare.pockettrainer;public class CalendarAdapter {
+}
---
+++ Codigo/app/src/main/java/com/softcare/pockettrainer/HistorialDiaActivity.java
@@ -0,0 +1,14 @@
+package com.softcare.pockettrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistorialDiaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial_dia);
+    }
+}
