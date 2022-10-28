b798c2d1f1f79c433d0be56b31be194d35751a94
Eric Cas <eric07blue@hotmail.com>
2022-10-27 20:43:22 -0500

    error ayudante bd solucionado

--- Codigo/app/src/main/java/com/softcare/pockettrainer/MainActivity.java
+++ Codigo/app/src/main/java/com/softcare/pockettrainer/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AyudanteBaseDeDatos ayudante = new AyudanteBaseDeDatos(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ayudante.agregarHorario();
