621b7da87af6cd26d55e5fc1cb029ae0f06cbfd7
Erick Sanchez Martinez <39204127@uaz.edu.mx>
2022-10-28 00:36:39 -0500

    Error base de datos corregido

--- Codigo/app/src/main/java/com/softcare/pockettrainer/MainActivity.java
+++ Codigo/app/src/main/java/com/softcare/pockettrainer/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AyudanteBaseDeDatos ayudante = new AyudanteBaseDeDatos(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AyudanteBaseDeDatos ayudante = new AyudanteBaseDeDatos(this);
